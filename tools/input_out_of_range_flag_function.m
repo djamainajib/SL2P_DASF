37ef4938ae8695ac109dcb45f23d345af57d7c4a
djamainajib <33295871+djamainajib@users.noreply.github.com>
2019-05-30 14:54:57 -0400

    Add files via upload

---
+++ tools/input_out_of_range_flag_function.m
@@ -0,0 +1,18 @@
+function [flag]=input_out_of_range_flag_function (data,r,c)
+
+h = waitbar(0,'Creating input out of range flag...');
+convex_hull=importdata('aux_data\S2_SL2P_large_trunc_u_D_convex_hull.mat');
+
+Nb_Cas=length(data);
+CL=ceil((data-repmat(convex_hull.Extreme(1,:),Nb_Cas,1))./repmat(convex_hull.Extreme(2,:)-convex_hull.Extreme(1,:),Nb_Cas,1).*convex_hull.N_classes);
+CL(CL>99)=99;
+UCL=0;
+for ii=1:size(CL,2),
+    UCL=UCL+CL(:,ii)*(100^(ii-1));
+end;
+
+flag=reshape(~ismember(UCL,convex_hull.data),r,c);
+end
+
+
+
