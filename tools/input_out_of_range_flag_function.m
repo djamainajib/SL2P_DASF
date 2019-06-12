a415dfe7403ed1ff89854081a99164098dfe9cbd
djamainajib <33295871+djamainajib@users.noreply.github.com>
2019-06-12 16:06:36 -0400

    Update input_out_of_range_flag_function.m

--- tools/input_out_of_range_flag_function.m
+++ tools/input_out_of_range_flag_function.m
@@ -1,5 +1,5 @@
 function [flag]=input_out_of_range_flag_function (data,r,c)
-convex_hull=importdata('aux_data\S2_SL2P_convex_hull_2.mat');
+convex_hull=importdata('aux_data\S2_SL2P_large_trunc_u_D_convex_hull.mat');
 Nb_Cas=length(data);
 CL=ceil((data-repmat(convex_hull.Extreme(1,:),Nb_Cas,1))./repmat(convex_hull.Extreme(2,:)-convex_hull.Extreme(1,:),Nb_Cas,1).*convex_hull.N_classes);
 CL(CL>99)=99;
